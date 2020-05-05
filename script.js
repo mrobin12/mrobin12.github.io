0ba8253b5f9c1ec03dca69e03eba1d2d93280ee6
mrobin12 <mrobin12@tufts.edu>
2020-05-05 08:04:40 -0400

    updated xml request endpoint

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var request = new XMLHttpRequest()
-request.open('GET', 'https://www.strava.com/oauth/authorize', true)
+request.open('GET', 'https://www.strava.com/athletes/290433?access_token=2a556674325dc9297b06a62435678d2222b55837', true)
 
 request.onload = function() {
     // Access JSON data here
