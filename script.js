e95277f042ccc62629b77975cf3d6ae1b62db8cd
mrobin12 <mrobin12@tufts.edu>
2020-05-13 07:32:10 -0400

    updated xml request, want to test deployed

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-var request = new XMLHttpRequest()
-request.open('GET', 'https://www.strava.com/athletes/290433?access_token=2a556674325dc9297b06a62435678d2222b55837', true)
+var customXMLHttpRequest = (function (myToken) {
+    function getXMLHttpRequest (method, url, async) {
+        console.log("here here here")
+        console.log("method: " + method)
+        console.log("url: " + url)
+        console.log("myToken: " + myToken)
+        var xmlHttpRequest = new XMLHttpRequest();
+        xmlHttpRequest.open(method, url, async);
+        xmlHttpRequest.setRequestHeader('Authorization', 'Bearer ' + myToken);
+        return xmlHttpRequest;
+    }
+    console.log("What about here?")
+    return getXMLHttpRequest;
+})('c44a4d563c34471db0f8ee1be61053daa5863142');
+//request.open('GET', 'https://www.strava.com/api/v3/athlete', true)
 
-request.onload = function() {
-    // Access JSON data here
-}
+//request.onload = function() {
+//    // Access JSON data here
+//}
 
+var request = customXMLHttpRequest('GET', 'https://www.strava.com/api/v3/athlete', true);
+console.log("am I here first?")
 request.send()
